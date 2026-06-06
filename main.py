2a9aabf3cfcfefd96a4dc87bd3bb7fa88104531c
lawrenceshi <108813113+lawrenceshi@users.noreply.github.com>
2026-06-06 18:57:32 -0400

    Fix: Use a better way to detect the user's operating system.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 import tkinter as tk
 from tkinter import ttk, messagebox
 import psutil
+import platform
+import os
 # , or lazy import in python 3.15
 # The main file of https://github.com/0-duke/wdpassport-utils includes a special symbol, using the power of the internet and AI, I found out I should use importlib.
 import importlib
@@ -13,6 +15,11 @@ class main(tk.Tk): # tk.Tk is a class, and main is a child class of it.
     def __init__(self): 
         super().__init__() # 所以这里是调用tk.Tk的初始化
 
+        if platform.system() != "Linux":
+            messagebox.WARNING(title = "Warning", text = "If you are not using Linux, please use the offical WD Drive Unlock program.")
+            if not messagebox.askyesno(title= "Warning", text = "Are you using Linux?"):
+                return 0
+        
         self.title("WdPassport-Unlocker")
         self.geometry("600x800")
         self.resizable(False, False)
@@ -20,15 +27,11 @@ class main(tk.Tk): # tk.Tk is a class, and main is a child class of it.
 
         self.create_main_interface()
     def create_main_interface(self):
-        
-        breakpoint()
 
         removable_device_names = []
 
         for d in psutil.disk_partitions(all=False):
-            if ":\\" in d.mountpoint:
-                messagebox.WARNING("You are using Windows, please use the offical program.")
-            elif "/run/media/" not in d.mountpoint:
+            if "/run/media/" not in d.mountpoint:
                 removable_device_names.append(d)
     
         unlock_butten = ttk.Button(text="Unlock",command=self.unlock_disk)
