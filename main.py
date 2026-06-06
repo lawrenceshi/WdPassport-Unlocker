02e3f81c75bd56d0de2ee6dadd7d7020953a3817
lawrenceshi <108813113+lawrenceshi@users.noreply.github.com>
2026-06-06 18:41:00 -0400

    Add: Detect removable devices on Linux

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # GitHub : https://github.com/lawrenceshi/WdPassport-Unlocker
 # License: GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
 import tkinter as tk
-from tkinter import ttk
-
+from tkinter import ttk, messagebox
+import psutil
+# , or lazy import in python 3.15
 # The main file of https://github.com/0-duke/wdpassport-utils includes a special symbol, using the power of the internet and AI, I found out I should use importlib.
 import importlib
 WdPassportUtils = importlib.import_module("WdPassportUtils.wdpassport-utils") #very important basic package
@@ -20,14 +21,25 @@ class main(tk.Tk): # tk.Tk is a class, and main is a child class of it.
         self.create_main_interface()
     def create_main_interface(self):
         
+        breakpoint()
+
+        removable_device_names = []
+
+        for d in psutil.disk_partitions(all=False):
+            if ":\\" in d.mountpoint:
+                messagebox.WARNING("You are using Windows, please use the offical program.")
+            elif "/run/media/" not in d.mountpoint:
+                removable_device_names.append(d)
+    
         unlock_butten = ttk.Button(text="Unlock",command=self.unlock_disk)
         unlock_butten.place(relx=0.5, rely=0.5)
-
 
         self.mainloop()
 
     def unlock_disk(self):
+        
         WdPassportUtils.unlock() # Test code, not using GUI.
+
 
 if __name__ == "__main__":
     main()
